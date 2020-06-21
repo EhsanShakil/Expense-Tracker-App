0a4e66e7780478d3aa6b6d762735638423c24280
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-21 16:24:46 +0500

    Adding Functionality in Balance Component

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,10 +1,14 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { GlobalCOntext, GlobalContext } from "../Context/GlobalState";
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amount = transactions.map((transaction) => transaction.amount);
+  const total = amount.reduce((total, item) => (total += item), 0).toFixed(2);
   return (
     <div>
       <h2>Your Balance</h2>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
